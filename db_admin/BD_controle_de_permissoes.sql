8cfc29925a6752d5235ac019c3518f6b10b2b5df
Cassio Esteves <cassio.esteves@hotmail.com>
2023-10-10 19:08:24 -0300

    Controle de permissões para bancos de dados, tabelas e colunas

---
+++ db_admin/BD_controle_de_permissoes.sql
@@ -0,0 +1,8 @@
+-- 161. Controle de permissões para bancos de dados, tabelas e colunas
+
+/* usuários e privilégios */
+select * from mysql.user;
+select * from mysql.db;
+select * from mysql.tables_priv;
+select * from mysql.columns_priv;
+select * from mysql.procs_priv;
